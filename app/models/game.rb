269bdfff803c0ed73725d900aad23faceec3fd13
razor <razorcd@yahoo.com>
2016-06-25 00:48:56 +0200

    adds two frame functionality to game model

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,19 +1,20 @@
 class Game < ActiveRecord::Base
   serialize :frames, Array
 
-  after_initialize do
-    self.frames = [[]]
-  end
-
   before_save do
     self.score = frames.flatten.sum
   end
 
   def throw! knocked_pins:
+    frames << [] if frame_completed?(frames.last)
     frames.last << knocked_pins
     save!
   end
 
 private
 
+  def frame_completed? frame
+    return true if (frame.nil? || frame.size==2 || frame[0]==10)
+    false
+  end
 end
--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe Game, type: :model do
   it "should have score and frames" do
     expect(Game.new.score).to eq 0
-    expect(Game.new.frames).to eq [[]]
+    expect(Game.new.frames).to eq []
   end
 
   context "one frame" do
@@ -19,5 +19,19 @@ RSpec.describe Game, type: :model do
     end
   end
 
+  context "two frames" do
+    let(:game) { Game.new }
+
+    it "should show correct score" do
+      game.throw! knocked_pins: 4
+      game.throw! knocked_pins: 3
+
+      game.throw! knocked_pins: 5
+      expect(game.score).to eq 12
+      game.throw! knocked_pins: 2
+      expect(game.score).to eq 14
+      expect(game.frames).to eq [[4,3], [5,2]]
+    end
+  end
 
 end
